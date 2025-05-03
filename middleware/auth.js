295bb033002e745951935fcc29b8c23e99d30651
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-05-04 01:28:08 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,51 +4,52 @@ const Admin = require("../models/admin");
 
 const auth = async (req, res, next) => {
   try {
-    const token =
-      req.headers?.authorization?.split(" ")[1] ||
-      req.cookies?.access_token?.split(" ")[1];
+
+    const token = req.headers?.authorization?.split(" ")[1] || req.cookies?.access_token;
 
     if (!token) {
       return res.status(401).send("Please login!");
     }
 
     const SECRETKEY = process.env.SECRETKEY;
-    const result = jwt.verify(token, SECRETKEY, { complete: true });
+  
+    const decoded = jwt.verify(token, SECRETKEY);
 
-    const user = await User.findById(result.payload.id);
+    const user = await User.findById(decoded.id);
     if (!user) {
       return res.status(401).send("User not found!");
     }
 
-    req.user = user;
-    next();
+    req.user = user; 
+    next(); 
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).send(e.message); 
   }
 };
 
 const adminAuth = async (req, res, next) => {
   try {
-    const token =
-      req.headers?.authorization?.split(" ")[1] ||
-      req.cookies?.access_token?.split(" ")[1];
+
+    const token = req.headers?.authorization?.split(" ")[1] || req.cookies?.access_token;
 
     if (!token) {
       return res.status(401).send("Please login!");
     }
 
     const SECRETKEY = process.env.SECRETKEY;
-    const result = jwt.verify(token, SECRETKEY, { complete: true });
 
-    const admin = await Admin.findById(result.payload.id);
+    const decoded = jwt.verify(token, SECRETKEY);
+
+   
+    const admin = await Admin.findById(decoded.id);
     if (!admin) {
       return res.status(401).send("Admin not found!");
     }
 
-    req.user = admin;
-    next();
+    req.user = admin; // إضافة المسؤول إلى الطلب
+    next(); // الانتقال إلى الخطوة التالية
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).send(e.message); // معالجة الأخطاء
   }
 };
 
