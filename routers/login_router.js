5261f7ec34d834d5d2b4a8439e3e5bfafd20de09
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-05-03 23:02:03 +0300

    Update login_router.js

--- routers/login_router.js
+++ routers/login_router.js
@@ -53,6 +53,37 @@ const Login = async (req, res) => {
   }
 };
 
+const logout = async (req, res) => {
+  try {
+    const token =
+      req.headers?.authorization?.split(" ")[1] ||
+      req.cookies?.access_token?.split(" ")[1];
 
+    if (!token) {
+      return res.status(401).json({ message: "لا يوجد توكن." });
+    }
 
-module.exports = Login;
+   
+    let user = await User.findOne({ tokens: token });
+    let userType = "user";
+
+    if (!user) {
+      user = await Admin.findOne({ tokens: token });
+      userType = "admin";
+    }
+
+    if (user) {
+      user.tokens = user.tokens.filter((t) => t !== token);
+      await user.save();
+    }
+
+  
+    res.clearCookie("access_token");
+
+    res.status(200).json({ message: "تم تسجيل الخروج بنجاح!" });
+  } catch (err) {
+    res.status(500).json({ message: "حدث خطأ أثناء تسجيل الخروج", error: err.message });
+  }
+};
+
+module.exports = {Login, logout};
