33b1e6c70c0a765f94774e6e957cb4bb11173be8
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-05-04 04:08:47 +0300

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,9 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 
-const url='mongodb+srv://moelmala086:cxHq8hqBayLyNuKj@clusteric.pkcjzzn.mongodb.net/?retryWrites=true&w=majority&appName=ClusterIC'
-
-
+const URL='mongodb+srv://moelmala086:cxHq8hqBayLyNuKj@clusteric.pkcjzzn.mongodb.net/?retryWrites=true&w=majority&appName=ClusterIC'
 const DBconnection =()=>{ 
-mongoose.connect(url)
+mongoose.connect(URL)
 .then(()=>{console.log('done connection !!')})
 .catch((e)=>{console.log(e.message)})
 }
