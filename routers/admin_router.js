20d129b2039ec36884f05f65732def5f1e3d5145
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-05-07 02:25:21 +0300

    Update admin_router.js

--- routers/admin_router.js
+++ routers/admin_router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { adminAuth } = require('../middleware/auth');
 const {Upload} = require('../middleware/uploads')
-    
+const { setLanguage } = require('../middleware/setLanguage'); 
+
 const { 
   admin_Register,
   
@@ -36,32 +37,32 @@ const {
 
 
 
-router.post('/admin_register',admin_Register)
+router.post('/admin_register',setLanguage,admin_Register)
 
-router.post('/add_main_product',adminAuth,Upload.any(),add_main_product)
-router.post('/add_versions_and_models_in_product/:product_id',adminAuth,add_versions_and_models_in_product)
-router.post('/add_services_in_model/:product_id/:version_id/:model_id',adminAuth,Upload.any(),add_services_in_model)
+router.post('/add_main_product',adminAuth,setLanguage,Upload.any(),add_main_product)
+router.post('/add_versions_and_models_in_product/:product_id',adminAuth,setLanguage,add_versions_and_models_in_product)
+router.post('/add_services_in_model/:product_id/:version_id/:model_id',adminAuth,setLanguage,Upload.any(),add_services_in_model)
 
-router.patch('/edit_main_product/:product_id',adminAuth,Upload.any(),edit_main_product)
-router.patch('/edit_versions_in_product/:product_id/:version_id',adminAuth,edit_versions_in_product)
-router.patch('/edit_model_and_service_in_version/:product_id/:version_id/:model_id/:service_id',adminAuth,Upload.any(),edit_model_and_service_in_version)
+router.patch('/edit_main_product/:product_id',adminAuth,setLanguage,Upload.any(),edit_main_product)
+router.patch('/edit_versions_in_product/:product_id/:version_id',adminAuth,setLanguage,edit_versions_in_product)
+router.patch('/edit_model_and_service_in_version/:product_id/:version_id/:model_id/:service_id',adminAuth,setLanguage,Upload.any(),edit_model_and_service_in_version)
 
-router.get('/get_all_supported_comman_reapir_devices',adminAuth,get_all_supported_comman_reapir_devices)
-router.get('/get_all_versions_and_models_in_product/:product_id',adminAuth,get_all_versions_and_models_in_product)
-router.get('/get_all_versions_in_product/:product_id',adminAuth,get_all_versions_in_product)
-router.get('/get_all_models_in_version/:product_id/:version_id',adminAuth,get_all_models_in_version)
-router.get('/get_all_services_in_model/:product_id/:version_id/:model_id',adminAuth,get_all_services_in_model)
+router.get('/get_all_supported_comman_reapir_devices',adminAuth,setLanguage,get_all_supported_comman_reapir_devices)
+router.get('/get_all_versions_and_models_in_product/:product_id',adminAuth,setLanguage,get_all_versions_and_models_in_product)
+router.get('/get_all_versions_in_product/:product_id',adminAuth,setLanguage,get_all_versions_in_product)
+router.get('/get_all_models_in_version/:product_id/:version_id',adminAuth,setLanguage,get_all_models_in_version)
+router.get('/get_all_services_in_model/:product_id/:version_id/:model_id',adminAuth,setLanguage,get_all_services_in_model)
 
-router.delete('/delete_product/:product_id',adminAuth,delete_product)
-router.delete('/delete_all_products',adminAuth,delete_all_products)
-router.delete('/delete_version_from_product/:product_id/:version_id',adminAuth,delete_version_from_product)
-router.delete('/delete_model_from_version/:product_id/:version_id/:model_id',adminAuth,delete_model_from_version)
-router.delete('/delete_service_from_model/:product_id/:version_id/:model_id/:service_id',adminAuth,delete_service_from_model)
+router.delete('/delete_product/:product_id',adminAuth,setLanguage,delete_product)
+router.delete('/delete_all_products',adminAuth,setLanguage,delete_all_products)
+router.delete('/delete_version_from_product/:product_id/:version_id',adminAuth,setLanguage,delete_version_from_product)
+router.delete('/delete_model_from_version/:product_id/:version_id/:model_id',adminAuth,setLanguage,delete_model_from_version)
+router.delete('/delete_service_from_model/:product_id/:version_id/:model_id/:service_id',adminAuth,setLanguage,delete_service_from_model)
 
 // لسه
-router.get('/get_imports',adminAuth,get_imports)
-router.delete('/delete_imports/:import_id',adminAuth,delete_imports)
-router.delete('/delete_all_imports',adminAuth,delete_all_imports)
+router.get('/get_imports',adminAuth,setLanguage,get_imports)
+router.delete('/delete_imports/:import_id',adminAuth,setLanguage,delete_imports)
+router.delete('/delete_all_imports',adminAuth,setLanguage,delete_all_imports)
 
 
 
