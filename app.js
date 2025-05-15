7cbc26d5dfbe0e7557a25c016b2aafe6ba20886d
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-05-15 17:25:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(setLanguage);
 app.use(compression()); 
 app.use(cors({
   
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://icmobile.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
