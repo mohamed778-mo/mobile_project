c26355c09d38d1a641581a51258872514df77e38
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-05-16 22:19:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const app = express();
 app.use(setLanguage); 
 app.use(compression()); 
 app.use(cors({
-    origin: "https://icmobile.netlify.app", 
+    origin: ["https://icmobile.netlify.app","http://localhost:3000"], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
